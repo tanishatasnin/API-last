5f288ea44dce0df5ce5737aa14a5d6edd28066a0
tanishatasin <tasnintanisha@gmail.com>
2021-09-01 19:56:00 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+const searchDrinks=()=>{
+               const searchfield=document.getElementById('search-feild')
+               const searchText=searchfield.value;
+               //console.log(searchText);
+               searchfield.value="";
+               const url=`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchText}`
+//console.log(url)
+
+fetch(url)
+.then(res=>res.json())
+.then(data=>displayDrinksResult(data.drinks));
+
+}
+
+const displayDrinksResult=(drinks)=>{
+const searchResult =document.getElementById('search-result');
+searchResult.textContent='';
+drinks.forEach(drink => {
+               console.log(drink);
+               //searchResult.value="";
+               const div=document.createElement('div');
+               div.classList.add('col');
+               div.innerHTML=`
+               <div onclick="lodeDrinksDItails(${drink.idDrink})" class="card">
+                   <img src="${drink.strDrinkThumb}" class="card-img-top" alt="...">
+                   <div class="card-body">
+                     <h5 class="card-title">${drink.strDrink}</h5>
+                     <p class="card-text">${drink.strInstructions.slice(0,100)}</p>
+                   </div>
+               `;
+
+               searchResult.appendChild(div);
+               
+});
+}
+const lodeDrinksDItails=idDrink=>{
+
+console.log(idDrink);
+const url=`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`;
+fetch(url)
+.then(res=>res.json())
+.then(data=>displayDrinksDItails(data.drinks[0]));
+
+}
+
+const displayDrinksDItails= drink=>{
+console.log(drink);
+const drinkDitails=document.getElementById('drink-details');
+const div = document.createElement('div');
+div.classList.add('card');
+div.innerHTML=`
+<img src="${drink.strDrinkThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${drink.strMeal}</h5>
+        <p class="card-text">${drink.strInstructions.slice(0, 150)}</p>
+        <a href="${drink.strYoutube}" class="btn btn-primary">Go somewhere</a>
+    </div>
+                              `;
+                              drinkDitails.appendChild(div);
+
+}
+
+
+
+
